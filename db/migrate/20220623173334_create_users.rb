04dc850936febbabbaf35ba9a37023314c0aa05b
deuro-ki <hero14yki1986@gmail.com>
2022-07-06 03:12:17 +0900

    Finish editing-users

--- db/migrate/20220623173334_create_users.rb
+++ db/migrate/20220623173334_create_users.rb
@@ -3,8 +3,11 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :name
       t.string :email
-
+      t.string :password_digest
+      t.string :remember_digest
+      t.boolean :admin, default: false
       t.timestamps
     end
+    add_index :users, :email, unique: true
   end
 end
