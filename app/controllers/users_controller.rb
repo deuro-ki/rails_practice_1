02e49ba9ae0d1febb07b81dbcb68542be91663f5
deuro-ki <hero14yki1986@gmail.com>
2022-07-07 01:28:12 +0900

    Finish add-basic-info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :logged_in_user, only: [:index, :show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: :destroy
+  before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
 
   def index
     @users = User.paginate(page: params[:page])
@@ -12,16 +12,15 @@ class UsersController < ApplicationController
   end
 
   def new
-    @user = User.new 
+    @user = User.new
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user # 保存成功後、ログインします。
+      log_in @user
       flash[:success] = '新規作成に成功しました。'
       redirect_to @user
-      # 保存に成功した場合は、ここに記述した処理が実行されます。
     else
       render :new
     end
@@ -32,11 +31,10 @@ class UsersController < ApplicationController
 
   def update
     if @user.update_attributes(user_params)
-      # 更新に成功した場合の処理を記述します。
       flash[:success] = "ユーザー情報を更新しました。"
       redirect_to @user
     else
-      render :edit
+      render :edit      
     end
   end
 
@@ -46,38 +44,53 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
-private
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  def edit_basic_info
+    
   end
 
-  # paramsハッシュからユーザーを取得します。
-  def set_user
-    @user = User.find(params[:id])
-  end
-
-  # ログイン済みのユーザーか確認します。
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "ログインしてください。"
-      redirect_to login_url
+  def update_basic_info
+    if @user.update_attributes(basic_info_params)
+      # 更新成功時の処理
+      flash[:success] = "#{@user.name}の基本情報を更新しました。"
+    else
+      flash[:danger] = "#{@user.name}の更新は失敗しました。" + @user.errors.full_messages.join("、")
     end
+    redirect_to users_url
   end
 
-  # アクセスしたユーザーが現在ログインしているユーザーか確認します。
-  def correct_user
-    #@user = User.find(params[:id])
-    #redirect_to(root_url) unless @user == current_user
-    redirect_to(root_url) unless current_user?(@user)
-  end
+  private
 
-  # システム管理権限所有かどうか判定します。
-  def admin_user
-    redirect_to root_url unless current_user.admin?
-  end
+    def user_params
+      params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
+    end
 
+    def basic_info_params
+      params.require(:user).permit(:department, :basic_time, :work_time)
+    end
 
+    # beforeフィルター
+
+    # paramsハッシュからユーザーを取得します。
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # ログイン済みのユーザーか確認します。
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "ログインしてください。"
+        redirect_to login_url
+      end
+    end
+
+    # アクセスしたユーザーが現在ログインしているユーザーか確認します。
+    def correct_user
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    # システム管理権限所有かどうか判定します。
+    def admin_user
+      redirect_to root_url unless current_user.admin?
+    end
 end
-
--- db/migrate/20220623173334_create_users.rb
+++ db/migrate/20220623173334_create_users.rb
@@ -6,6 +6,9 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.string :password_digest
       t.string :remember_digest
       t.boolean :admin, default: false
+      t.string :department
+      t.datetime :basic_time, default: Time.current.change(hour: 8, min: 0, sec: 0)
+      t.datetime :work_time, default: Time.current.change(hour: 7, min: 30, sec: 0)
       t.timestamps
     end
     add_index :users, :email, unique: true
